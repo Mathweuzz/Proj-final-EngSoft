c774941e4148effd0e5991d70798cc7acbf28ea0
edumpda <edumarques.pereira@gmail.com>
2023-07-09 15:42:24 -0300

    update app

--- app.py
+++ app.py
@@ -9,7 +9,15 @@ db = SQLAlchemy(app)
 
 @app.route('/')
 def home():
-    return "Hello World!"
+    return render_template("index.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
