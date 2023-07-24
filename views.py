8a29ac4baef0a3419b7316c339653a68d897599c
yantavares <yantdo1@gmail.com>
2023-07-23 23:56:06 -0300

    deleted unused imports

--- views.py
+++ views.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, jsonify, session, redirect, url_for
 from flask_cors import CORS
-from controllers import seed_data, registro, login, create_exam, responder_exame, relatorio_exame, create_question, close_exam, visualizar_resultados, avaliar_exame
-from models import User, Question, Answer, Exam, db  
+from controllers import seed_data, registro, login, create_exam, create_question, close_exam, avaliar_exame
+from models import Question, Answer, Exam, db
 import secrets
 
 
@@ -43,6 +43,7 @@ def login_route():
 def create_exam_route():
     return create_exam()
 
+
 @app.route('/questions', methods=['POST'])
 def create_question_route():
     return create_question()
@@ -52,13 +53,17 @@ def create_question_route():
 def close_exam_route(exame_id):
     return close_exam(exame_id)
 
+
 @app.route('/exames/<int:exame_id>/avaliar', methods=['POST'])
 def evaluate_exam_route(exame_id):
     return avaliar_exame(exame_id)
 
+
 CORS(app, resources={r"/get_questions": {"origins": "*"}})
 
 # Rota para obter as questões em formato JSON
+
+
 @app.route('/get_questions', methods=['GET', 'POST'])
 def get_questions():
     try:
@@ -74,8 +79,10 @@ def get_questions():
             pass
     except Exception as e:
         return jsonify({'error': str(e)}), 500
- 
+
 # Route to render the student dashboard
+
+
 @app.route('/dashboard_student')
 def student_dashboard():
     return render_template('aluno.html', css_file='aluno.css')
@@ -96,7 +103,8 @@ def responder_exame_route(exame_id):
 
     # Verificar se o aluno já respondeu o exame anteriormente
     user_id = session['usuario_id']
-    user_has_answered = Answer.query.filter_by(exame_id=exame_id, user_id=user_id).first()
+    user_has_answered = Answer.query.filter_by(
+        exame_id=exame_id, user_id=user_id).first()
 
     if user_has_answered:
         return jsonify({"error": "Você já respondeu esse exame. Não é permitido responder novamente."})
@@ -137,7 +145,8 @@ def responder_exame_route(exame_id):
         db.session.add(nova_resposta)
 
     # Atualizar a pontuação total do aluno para o exame no modelo Answer
-    user_answers = Answer.query.filter_by(exame_id=exame_id, user_id=user_id).all()
+    user_answers = Answer.query.filter_by(
+        exame_id=exame_id, user_id=user_id).all()
     total_score_user = sum(answer.pontuacao for answer in user_answers)
 
     # Atualizar a pontuação total do exame no modelo Exam
@@ -149,6 +158,8 @@ def responder_exame_route(exame_id):
     return jsonify({"feedback": feedback, "total_score": total_score})
 
 # Route to generate the exam report for the student
+
+
 @app.route('/exames/<int:exame_id>/relatorio', methods=['GET'])
 def exam_report_route(exame_id):
     exame = Exam.query.get(exame_id)
@@ -156,7 +167,8 @@ def exam_report_route(exame_id):
     if not exame:
         return jsonify({"error": "O exame não foi encontrado"})
 
-    user_id = session.get('usuario_id')  # Retrieve the user_id from the session
+    # Retrieve the user_id from the session
+    user_id = session.get('usuario_id')
 
     if 'professor' in session.get('profile'):
         print('teste')
@@ -197,39 +209,46 @@ def exam_report_route(exame_id):
         return jsonify({"respostas": respostas})
 
 # Route to load questions based on the exam ID
+
+
 @app.route('/exames/<int:exame_id>/questions', methods=['GET'])
 def load_exam_questions(exame_id):
     exame = Exam.query.get(exame_id)
 
     if not exame:
         return jsonify({"error": "O exame não foi encontrado"})
-    
+
     if exame.status == 'encerrado':
         return jsonify({"error": "O exame já foi encerrado. Não é permitido responder novamente."})
 
     questions = exame.questions
-    questions_data = [{"id": question.id, "question": question.question} for question in questions]
+    questions_data = [{"id": question.id, "question": question.question}
+                      for question in questions]
 
     return jsonify({"questions": questions_data})
+
 
 @app.route('/exames/<int:exame_id>/respondeu', methods=['GET'])
 def check_exam_answered(exame_id):
     user_id = session.get('usuario_id')
     if not user_id:
-        return jsonify({"respondeu": False})  # O aluno não está logado, então ainda não respondeu
+        # O aluno não está logado, então ainda não respondeu
+        return jsonify({"respondeu": False})
 
-    exame_respondido = Answer.query.filter_by(exame_id=exame_id, user_id=user_id).first()
+    exame_respondido = Answer.query.filter_by(
+        exame_id=exame_id, user_id=user_id).first()
     if exame_respondido:
         return jsonify({"respondeu": True})  # O aluno já respondeu o exame
     else:
-        return jsonify({"respondeu": False})  # O aluno ainda não respondeu o exame
-
+        # O aluno ainda não respondeu o exame
+        return jsonify({"respondeu": False})
 
 
 @app.route('/logout', methods=['GET', 'POST'])
 def logout():
     session.pop('usuario_id', None)
     return redirect(url_for('login_route'))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
